3d945085d81af155aa1a99bc3c87c615434257fb
mrmazyboy <mrmazyboy@gmail.com>
2023-03-27 16:44:15 +0500

    added main menu

---
+++ js/header.js
@@ -0,0 +1,58 @@
+const burger = document.querySelector(".header-main__burger");
+const mainMenu = document.querySelector(".main-menu");
+burger.addEventListener("click", () => {
+  mainMenu.classList.add("--active");
+});
+
+const mainMenuCloseButton = document.querySelector(
+  ".main-menu__controls-button"
+);
+mainMenuCloseButton.addEventListener("click", () => {
+  mainMenu.classList.remove("--active");
+});
+// burger------------------------
+
+const controls = document
+  .querySelector("#js-main-menu-controls")
+  .querySelectorAll(".main-menu__item");
+const menuContents = document.querySelectorAll(".main-menu__second-level");
+const mainMenuContentContainer = document.querySelector(
+  ".main-menu__content-container"
+);
+
+const brakePoint = 767.98;
+
+const handleClickControlsItem = (idx) => {
+  const windowWidth = window.innerWidth;
+  for (let i = 0; i < controls.length; i++) {
+    if (idx === i) {
+      controls[i].classList.add("--active");
+      menuContents[i].classList.add("--active");
+    } else {
+      controls[i].classList.remove("--active");
+      menuContents[i].classList.remove("--active");
+    }
+  }
+  console.log(brakePoint);
+  console.log(windowWidth);
+
+  if (brakePoint > windowWidth) {
+    console.log(mainMenuContentContainer);
+    mainMenuContentContainer.classList.add("--active");
+  }
+};
+
+for (let i = 0; i < controls.length; i++) {
+  controls[i].addEventListener("click", () => {
+    handleClickControlsItem(i);
+  });
+  controls[0].classList.add("--active");
+  menuContents[0].classList.add("--active");
+}
+
+const secondLevelCrossButton = document.querySelector(
+  ".main-menu__content-button"
+);
+secondLevelCrossButton.addEventListener("click", () => {
+  mainMenuContentContainer.classList.remove("--active");
+});
