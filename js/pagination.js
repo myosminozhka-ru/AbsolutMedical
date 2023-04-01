9ab45d87be6219e56ee05aec1661382d2b6be87c
mrmazyboy <mrmazyboy@gmail.com>
2023-04-01 17:15:11 +0500

    added license page

---
+++ js/pagination.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const numbers = [1, 2, 3, 4, 5, 22];
+  const prev = document.querySelector(".ui-pagination-prev");
+  const next = ducument.querySelector(".ui-pagination-next");
+  const paginationNumbers = document.querySelectorAll(".ui-pagination-number");
+  let activeNumber = 1;
+
+  const setNumbers = function () {};
+
+  const fetchPrev = function () {};
+
+  const fetchNext = function () {};
+
+  const fetchPageById = async function (id) {
+    try {
+      const response = await 1;
+      setNumbers(id);
+    } catch (e) {
+      console.error("error fetching page");
+    }
+  };
+
+  if (prev && next && paginationNumbers) {
+    prev.addEventListener("click", function () {
+      fetchPrev();
+    });
+
+    next.addEventListener("click", function () {
+      fetchNext();
+    });
+
+    for (let i = 1; i <= paginationNumbers.length; i++) {
+      paginationNumbers[i].addEventListener("click", function () {
+        fetchPageById(i);
+      });
+    }
+  }
+});
